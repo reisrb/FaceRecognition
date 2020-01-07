04d24e4e86b4d90b0ae880b3458d6b0bcbb184c7
rafaelreisbandtec <rafael.reis@bandtec.com.br>
2020-01-06 22:32:55 -0300

    capturando amostras de rostos

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -1,26 +1,25 @@
 import numpy as np
 import cv2
+import os
 
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-smileCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') #carregando os classificadores de detectação do rosto em uma imagem
+eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml') #olhos
+smileCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml') #boca
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+face_id = input('\ n insira o ID do usuário e pressione ==>')
+
+cap = cv2.VideoCapture(0) #pegando a camera principal e retornando a variável
+cap.set(3, 800) #exibindo em um tamanho especificado
+cap.set(4, 600)
 
 while True:
-    ret, img = cap.read()
-    img = cv2.flip(img, 1)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(20, 20)
-    )
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+    ret, img = cap.read() #pegando a captura
+    img = cv2.flip(img, 1) #deixando na posição vertical normal
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #a imagem em escala cinza para detecção
+    faces = faceCascade.detectMultiScale(gray, 1.2, 3) #função classificadora: fator de escala, número de vizinhos e tamanho mínimo da face detectada = minSize = (20, 20).
+
+    for (x, y, w, h) in faces: #detectar rostos na imagem marcando cada face com um retangulo azul
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3) #onde pegando a posição da largura e altura da minha imagem será setada o retangulo dando coordenadas x,y
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
 
@@ -28,14 +27,14 @@ while True:
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-        #smile = smileCascade.detectMultiScale(roi_gray)
-        #for (ex, ey, ew, eh) in smile:
-            #cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
+        smile = smileCascade.detectMultiScale(roi_gray)
+        for (sx, sy, sw, sh) in smile:
+            cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (0, 0, 255), 2)
 
-    cv2.imshow('video', img)
+    cv2.imshow('video', img) #criando uma janela e mostrando na tela
 
-    k = cv2.waitKey(30) & 0xff
+    k = cv2.waitKey(30) & 0xff #tecla esc para sair
     if k == 27:
         break
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() #fecha
---
+++ FacialRecognition/Storage.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 640)
+cap.set(4, 480)
+
+face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+
+face_id = input('\n insira um id de usuário e pessione <enter> ==>  ')
+
+print("\n [INFO] iniciando captura de rosto. Olhe a câmera e aguarde ...")
+
+count = 0
+
+while(True):
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        count += 1
+
+        # Save the captured image into the datasets folder
+        cv2.imwrite("dataset/User." + str(face_id) + '.' +
+                    str(count) + ".jpg", gray[y:y+h, x:x+w])
+        cv2.imshow('image', img)
+
+    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+
+    if k == 27:
+        break
+    elif count >= 30:
+         break
+
+print("\n [INFO] saindo")
+cap.release()
+cv2.destroyAllWindows()
