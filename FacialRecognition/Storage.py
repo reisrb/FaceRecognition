edf0946aeb1d1d63c168aa0d82f5f0cfb76c8c6b
rafaelreisbandtec <rafael.reis@bandtec.com.br>
2020-01-06 22:39:53 -0300

    up

--- FacialRecognition/Storage.py
+++ FacialRecognition/Storage.py
@@ -1,10 +1,10 @@
 import cv2
 import os
 
-cap = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0)
 
-cap.set(3, 640)
-cap.set(4, 480)
+cam.set(3, 640)
+cam.set(4, 480)
 
 face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -16,7 +16,7 @@ print("\n [INFO] iniciando captura de rosto. Olhe a câmera e aguarde ...")
 count = 0
 
 while(True):
-    ret, img = cap.read()
+    ret, img = cam.read()
     img = cv2.flip(img, 1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, 1.3, 5)
@@ -26,7 +26,7 @@ while(True):
         count += 1
 
         # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/User." + str(face_id) + '.' +
+        cv2.imwrite("/dataset/User." + str(face_id) + '.' +
                     str(count) + ".jpg", gray[y:y+h, x:x+w])
         cv2.imshow('image', img)
 
@@ -38,5 +38,5 @@ while(True):
          break
 
 print("\n [INFO] saindo")
-cap.release()
+cam.release()
 cv2.destroyAllWindows()
